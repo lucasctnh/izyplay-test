89bbce857c5976b5448391718e98864b661dabdd
lcscout <lcscoutinho@outlook.com>
2022-05-01 18:45:37 -0300

    refactor(EndGameCollider): rename script

--- Assets/Scripts/World/EndGameCollider.cs
+++ Assets/Scripts/World/EndGameCollider.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndGame : MonoBehaviour {
-	public static event Action<int> OnEndGame;
+public class EndGameCollider : MonoBehaviour {
+	public static event Action<int> OnHit;
 
 	[Tooltip("The multiplier that will be applied to the current coins if player hits this object")]
     [SerializeField] private int _coinsMultiplier = 1;
 
 	private void OnCollisionEnter(Collision other) {
 		if (other.collider.gameObject.CompareTag("Blade"))
-			OnEndGame?.Invoke(_coinsMultiplier);
+			OnHit?.Invoke(_coinsMultiplier);
 	}
 }

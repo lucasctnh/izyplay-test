55dc46638a6f787fe43ab5a0fbb024642e5ade6d
lcscout <lcscoutinho@outlook.com>
2022-05-01 13:58:48 -0300

    feat(GameManager): add methods for skin change, load scene and pause

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -8,16 +8,20 @@ public class GameManager : MonoBehaviour {
 	public static GameManager Instance;
 
 	public static event Action<int> OnUpdateCoins;
+	public static event Action OnSkinChange;
 
-	public int Coins { get { return _coins; } }
+	public int Coins { get; private set; }
+	public int CurrentSkin { get; private set; }
 	public bool IsGameRunning { get { return _isGameRunning; } private set { _isGameRunning = value; } }
 	public bool HasGameStarted { get { return _hasGameStarted; } private set { _hasGameStarted = value; } }
 
-	[SerializeField] private int _targetFrameRate = 60;
-	[SerializeField] private Transform _playerSpawn;
-	[SerializeField] private GameObject _skinPrefab;
+	[Header("References")]
+	public Transform playerSpawn;
+	public List<GameObject> skins = new List<GameObject>();
 
-	private int _coins = 0;
+	[Header("Settings")]
+	[SerializeField] private int _targetFrameRate = 60;
+
 	private bool _isGameRunning = true;
 	private bool _hasGameStarted = false;
 
@@ -25,12 +29,15 @@ public class GameManager : MonoBehaviour {
 		UIManager.OnFirstTap += () => HasGameStarted = true;
 		EndGame.OnEndGame += (multiplier) => GameOver(multiplier);
 		Blade.OnBladeCut += IncreaseCoins;
+		PlayerController.OnDeath += StopGame;
+		UIManager.OnPause += (pauseState) => Pause(pauseState);
 	}
 
 	private void OnDisable() {
 		UIManager.OnFirstTap -= () => HasGameStarted = true;
 		EndGame.OnEndGame -= (multiplier) => GameOver(multiplier);
 		Blade.OnBladeCut -= IncreaseCoins;
+		UIManager.OnPause -= (pauseState) => Pause(pauseState);
 	}
 
 	private void Awake() {
@@ -45,19 +52,67 @@ public class GameManager : MonoBehaviour {
 		LimitFrameRate();
 	}
 
-	public void ChangeSkin() {
+	private void Start() => FindNewPlayerSpawn();
+
+	public void ChangeSkin(int skinIndex) {
 		GameObject player = GameObject.FindGameObjectWithTag("Player");
-		GameObject newPlayer = Instantiate(_skinPrefab, _playerSpawn.position, _skinPrefab.transform.rotation);
-		Camera.main.GetComponent<FollowTarget>().SetNewTarget(newPlayer.transform);
-		Destroy(player);
+		GameObject newPlayer = Instantiate(skins[skinIndex], playerSpawn.position, skins[skinIndex].transform.rotation);
+		CameraFollowNewTarget(newPlayer.transform);
+
+		if (player != null)
+			Destroy(player);
+
+		CurrentSkin = skinIndex;
+		OnSkinChange?.Invoke();
 	}
 
-	public void LoadNextLevel() {
+	public void CameraFollowNewTarget(Transform target) {
+		FollowTarget followScript = Camera.main.GetComponent<FollowTarget>();
+		if (followScript != null)
+			followScript.SetNewTarget(target);
+	}
+
+	public void LoadLevel(int index) => StartCoroutine(LoadScene(index));
+
+	public bool SpendCoins(int spendAmount) { // returns true if the buy was sucessful
+		if (Coins - spendAmount >= 0) {
+			Coins -= spendAmount;
+			OnUpdateCoins?.Invoke(Coins);
+
+			return true;
+		}
+		else
+			return false;
+	}
+
+	private IEnumerator LoadScene(int index) {
 		ResetGameState();
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+
+		AsyncOperation asyncLoadLevel = SceneManager.LoadSceneAsync(index, LoadSceneMode.Single);
+		while (!asyncLoadLevel.isDone)
+			yield return null;
+
+		yield return new WaitForEndOfFrame();
+
+		FindNewPlayerSpawn();
 	}
 
-	private void ResetGameState() => _isGameRunning = true;
+	private void FindNewPlayerSpawn() {
+		GameObject spawn = GameObject.FindGameObjectWithTag("Player Spawn");
+		if (spawn != null)
+			playerSpawn = spawn.transform;
+	}
+
+	private void ResetGameState() {
+		UnfreezeGame();
+
+		IsGameRunning = true;
+		HasGameStarted = false;
+	}
+
+	private void FreezeGame() => Time.timeScale = 0f;
+
+	private void UnfreezeGame() => Time.timeScale = 1f;
 
 	private void LimitFrameRate() {
 		QualitySettings.vSyncCount = 0;
@@ -65,17 +120,29 @@ public class GameManager : MonoBehaviour {
 	}
 
 	private void IncreaseCoins() {
-		_coins++;
-		OnUpdateCoins?.Invoke(_coins);
+		Coins++;
+		OnUpdateCoins?.Invoke(Coins);
 	}
 
 	private void GameOver(int multiplier) {
-		IsGameRunning = false;
+		StopGame();
 		MultiplyCoins(multiplier);
 	}
 
+	private void StopGame() {
+		IsGameRunning = false;
+		FreezeGame();
+	}
+
 	private void MultiplyCoins(int multiplier) {
-		_coins *= multiplier;
-		OnUpdateCoins?.Invoke(_coins);
+		Coins *= multiplier;
+		OnUpdateCoins?.Invoke(Coins);
+	}
+
+	private void Pause(bool pauseState) {
+		if (pauseState)
+			FreezeGame();
+		else
+			UnfreezeGame();
 	}
 }

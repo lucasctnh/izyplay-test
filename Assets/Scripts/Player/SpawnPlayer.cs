cdae58244f43fa8e3d7e3363994492d05918a2da
lcscout <lcscoutinho@outlook.com>
2022-05-01 18:30:07 -0300

    refactor(SpawnPlayer): put spawn logic into a public method

--- Assets/Scripts/Player/SpawnPlayer.cs
+++ Assets/Scripts/Player/SpawnPlayer.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnPlayer : MonoBehaviour {
-	private void Start() {
+	private void Start() => SpawnNewPlayer();
+
+	public void SpawnNewPlayer() {
 		List<GameObject> skins = GameManager.Instance.skins;
 		int currentSkin = GameManager.Instance.CurrentSkin;
-		GameObject newPlayer = Instantiate(skins[currentSkin], transform.position, skins[currentSkin].transform.rotation);
 
+		GameObject newPlayer = Instantiate(skins[currentSkin], transform.position, skins[currentSkin].transform.rotation);
 		GameManager.Instance.CameraFollowNewTarget(newPlayer.transform);
 	}
 }
